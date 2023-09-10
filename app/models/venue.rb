8564558a74b3ed88a9cbf8c0541703c78ec8945e
rishi monohur <rishi.monohur15@gmail.com>
2023-09-10 07:55:26 +0400

    users and venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,5 +1,8 @@
 class Venue < ApplicationRecord
   belongs_to :location
   has_many :reviews
-  belongs_to :booking
+  # belongs_to :booking
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
