223c967f62eb054aca0a0151298d58621e110c45
rishi monohur <rishi.monohur15@gmail.com>
2023-09-14 00:24:08 +0400

    ajaxified the ai suggestion

--- app/javascript/controllers/display_suggestions_controller.js
+++ app/javascript/controllers/display_suggestions_controller.js
@@ -2,8 +2,31 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="display-suggestions"
 export default class extends Controller {
+
+  static targets = ["form", "input", "abc", "list"];
+
+
   connect() {
     console.log("displaying suggestions..");
+    console.log("list", this.listTarget);
+  }
+
+  send(){
+    event.preventDefault()
+    console.log("send..");
+    console.log("input..", this.inputTarget);
+
+    console.log("input 123..", this.inputTarget.value);
+
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`
+    console.log("url" , url);
+    fetch(url, {
+      headers: { "Accept": "text/plain" }
+    })
+    .then(response => response.text())
+    .then((data) => {
+      this.listTarget.outerHTML = data
+    })
   }
 
 }
