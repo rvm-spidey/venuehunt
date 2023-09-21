e058ef946143c4fc7fc1b4ac19dca71c6888753c
Luchmee Toolseeram <luchmee.toolseeram@gmail.com>
2023-09-21 21:10:54 +0400

    Pulling latest changes

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,9 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -23,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
-
   create_table "active_storage_blobs", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
@@ -35,13 +32,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
-
   create_table "active_storage_variant_records", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-
   create_table "bookings", force: :cascade do |t|
     t.date "date_from"
     t.date "date_to"
@@ -55,10 +50,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.datetime "updated_at", null: false
     t.bigint "venue_id"
     t.bigint "cart_id"
+    t.string "status"
     t.index ["cart_id"], name: "index_bookings_on_cart_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
   end
-
   create_table "carts", force: :cascade do |t|
     t.integer "total"
     t.datetime "created_at", null: false
@@ -66,7 +61,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.bigint "user_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
-
+  create_table "chatrooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "admin_id"
+    t.index ["admin_id"], name: "index_chatrooms_on_admin_id"
+  end
   create_table "locations", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -77,7 +78,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.float "latitude"
     t.float "longitude"
   end
-
+  create_table "messages", force: :cascade do |t|
+    t.string "content"
+    t.bigint "chatroom_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chatroom_id"], name: "index_messages_on_chatroom_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
   create_table "orders", force: :cascade do |t|
     t.string "email_address"
     t.integer "card_number"
@@ -93,7 +102,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
-
   create_table "reviews", force: :cascade do |t|
     t.integer "rating"
     t.text "content"
@@ -108,7 +116,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.index ["user_id"], name: "index_reviews_on_user_id"
     t.index ["venue_id"], name: "index_reviews_on_venue_id"
   end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -127,7 +134,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
   create_table "venues", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -144,30 +150,31 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_170659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "location_id"
-    t.bigint "users_id"
+    t.bigint "user_id"
     t.bigint "venuetype_id"
     t.index ["location_id"], name: "index_venues_on_location_id"
-    t.index ["users_id"], name: "index_venues_on_users_id"
+    t.index ["user_id"], name: "index_venues_on_user_id"
     t.index ["venuetype_id"], name: "index_venues_on_venuetype_id"
   end
-
   create_table "venuetypes", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "bookings", "carts"
   add_foreign_key "bookings", "venues"
   add_foreign_key "carts", "users"
+  add_foreign_key "chatrooms", "users", column: "admin_id"
+  add_foreign_key "messages", "chatrooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "orders", "carts"
   add_foreign_key "orders", "users"
   add_foreign_key "reviews", "bookings"
   add_foreign_key "reviews", "users"
   add_foreign_key "reviews", "venues"
   add_foreign_key "venues", "locations"
-  add_foreign_key "venues", "users", column: "users_id"
+  add_foreign_key "venues", "users"
   add_foreign_key "venues", "venuetypes"
 end
